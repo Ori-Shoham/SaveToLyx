21f1f848d9c31e7df44b34962a699f8247cc9aec
Ori-Shoham <ori.shoham1@gmail.com>
2022-02-10 17:52:43 +0200

    Add save to lyx function

---
+++ R/save_to_lyx.R
@@ -0,0 +1,77 @@
+#' Title
+#'
+#' @param currentValue
+#' @param currentName
+#' @param latexFile
+#' @param translate
+#' @param digits
+#' @param percent
+#' @param accuracy
+#' @param dataFile
+#'
+#' @return
+#' @export
+#'
+#' @examples
+save_to_lyx <- function(currentValue, currentName, latexFile = dataLyxOutput, translate=TRUE,
+                      digits = 2, percent=FALSE, accuracy=1, dataFile = NULL) {
+  if(grepl("[0-9]", currentName)) {
+    stop("Can't include digits in name")
+  }
+
+  if (is.numeric(currentValue)) {
+    if (percent) {
+      currentValue = scales::percent(currentValue, accuracy=accuracy, big.mark = ",")
+    } else {
+      currentValue = formatC(currentValue, format = "f", digits = digits,  big.mark = ",")
+    }
+    if(grepl("[0-9]", currentName)) {
+      stop("Can't include digits in name")
+    }
+  }
+
+  if (translate==TRUE) {
+    currentValue = Hmisc::latexTranslate(currentValue)
+  }
+
+  # Get current values
+  if (!is.null(dataFile)) {
+    if (file.exists(dataFile)) {
+      DATA <- readRDS(dataFile)
+      DATA = DATA[name!=currentName]
+    } else {
+      DATA = data.table()
+      DATA = rbind(DATA, list(name=currentName, value=currentValue, lastUpdated = Sys.time()))
+    }
+  } else {
+    if (file.exists(latexFile) & file.size(latexFile)>10) {
+      DATA <- fread(latexFile, sep=" ", header = FALSE, col.names = c("name", "value"))
+      #DATA[, name:=gsub("\\\\newcommand\\\\", "", name)]
+      #DATA[, value:=gsub("^\\{", "", gsub("}$", "", value))]
+      DATA = DATA[gsub("\\\\newcommand\\\\", "", name)!=currentName]
+    } else {
+      DATA = data.table()
+    }
+    DATA = rbind(DATA, list(name=paste0("\\newcommand\\", currentName), value=paste0("{", currentValue ,"}")))
+  }
+
+  DATA = unique(DATA)
+  DATA = DATA[order(name)]
+  setkey(DATA, "name")
+
+  if (!is.null(dataFile)) {
+    saveRDS(DATA, file = dataFile)
+  }
+
+  first = 1; # iterateName=DATA$name[1]
+  for(iterateName in DATA$name) {
+    myAppend = FALSE
+    if (first==0) {
+      myAppend = TRUE
+    }
+
+    iterateValue = DATA[name==iterateName, value]
+    write(file=latexFile, paste0(iterateName, " ", iterateValue), append=myAppend)
+    first = 0
+  }
+}
