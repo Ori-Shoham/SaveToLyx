6f974eb3fc9da5800598f5236829161be5457936
Ori-Shoham <ori.shoham1@gmail.com>
2022-02-24 15:30:15 +0200

    Style

--- R/save_to_lyx.R
+++ R/save_to_lyx.R
@@ -13,19 +13,19 @@
 #' @import data.table
 #'
 #'
-save_to_lyx <- function(currentValue, currentName, latexFile = dataLyxOutput, translate=TRUE,
-                      digits = 2, percent=FALSE, accuracy=1, replaceValues = TRUE) {
+save_to_lyx <- function(currentValue, currentName, latexFile = dataLyxOutput, translate = TRUE,
+                        digits = 2, percent = FALSE, accuracy = 1, replaceValues = TRUE) {
   # Test for valid inputs
-  if(any(grepl("[^A-Za-z]", currentName))) {
+  if (any(grepl("[^A-Za-z]", currentName))) {
     stop("Names must consist of letters only")
   }
-  if(any(duplicated(currentName))){
+  if (any(duplicated(currentName))) {
     stop("Names must be unique")
   }
-  if(length(currentValue)!=length(currentName)){
+  if (length(currentValue) != length(currentName)) {
     stop("Names and Values must be of compatible lengths")
   }
-  if(length(percent) > 1 & length(percent)!=length(currentName)){
+  if (length(percent) > 1 & length(percent) != length(currentName)) {
     stop("percent must be of length 1 or of the same length as values and names")
   }
   n <- length(currentValue)
@@ -33,47 +33,45 @@ save_to_lyx <- function(currentValue, currentName, latexFile = dataLyxOutput, tr
   currentValueTemp <- vector("character", length = n)
   for (i in 1:n) {
     if (is.numeric(currentValue[i])) {
-      if ((percent+rep(0,n))[i]) {
-        currentValueTemp[i] = scales::percent(currentValue[i], accuracy=accuracy, big.mark = ",")
+      if ((percent + rep(0, n))[i]) {
+        currentValueTemp[i] <- scales::percent(currentValue[i], accuracy = accuracy, big.mark = ",")
       } else {
-        currentValueTemp[i] = formatC(currentValue[i], format = "f", digits = digits,  big.mark = ",")
+        currentValueTemp[i] <- formatC(currentValue[i], format = "f", digits = digits, big.mark = ",")
       }
     }
   }
   currentValue[is.numeric(currentValue)] <- currentValueTemp[is.numeric(currentValue)]
 
   if (translate) {
-    currentValue = Hmisc::latexTranslate(currentValue)
+    currentValue <- Hmisc::latexTranslate(currentValue)
   }
 
   # Get current values
-  if (file.exists(latexFile) & file.size(latexFile)>10) {
-      DATA <- data.table::fread(latexFile, sep=" ", header = FALSE, col.names = c("name", "value"))
-      n_exist <- DATA[gsub("\\\\newcommand\\\\", "", name) %in% currentName,.N]
-      if(!replaceValues & n_exist>0){
-        stop("currentName contains existing names")
-      }
-      DATA <-  DATA[!gsub("\\\\newcommand\\\\", "", name) %in% currentName]
-  } else {
-      DATA = data.table::data.table()
-      n_exist <- 0
+  if (file.exists(latexFile) & file.size(latexFile) > 10) {
+    DATA <- data.table::fread(latexFile, sep = " ", header = FALSE, col.names = c("name", "value"))
+    n_exist <- DATA[gsub("\\\\newcommand\\\\", "", name) %in% currentName, .N]
+    if (!replaceValues & n_exist > 0) {
+      stop("currentName contains existing names")
     }
-  DATA = rbind(DATA, list(name=paste0("\\newcommand\\", currentName), value=paste0("{", currentValue ,"}")))
+    DATA <- DATA[!gsub("\\\\newcommand\\\\", "", name) %in% currentName]
+  } else {
+    DATA <- data.table::data.table()
+    n_exist <- 0
+  }
+  DATA <- rbind(DATA, list(name = paste0("\\newcommand\\", currentName), value = paste0("{", currentValue, "}")))
 
-  DATA = unique(DATA)
-  DATA = DATA[order(name)]
+  DATA <- unique(DATA)
+  DATA <- DATA[order(name)]
   data.table::setkey(DATA, "name")
 
 
-  first = 1; # iterateName=DATA$name[1]
-  for(iterateName in DATA$name) {
-    myAppend <-  ifelse(first==0, TRUE, FALSE)
-    iterateValue = DATA[name==iterateName, value]
-    write(paste0(iterateName, " ", iterateValue), file=latexFile, append=myAppend)
-    first = 0
+  first <- 1 # iterateName=DATA$name[1]
+  for (iterateName in DATA$name) {
+    myAppend <- ifelse(first == 0, TRUE, FALSE)
+    iterateValue <- DATA[name == iterateName, value]
+    write(paste0(iterateName, " ", iterateValue), file = latexFile, append = myAppend)
+    first <- 0
   }
-  if (n_exist>1)  cat(paste0(n_exist," values replaced"))
-  if
+  if (n_exist > 1) cat(paste0(n_exist, " values replaced"))
+  if (n_exist == 1) cat("One value replaced")
 }
-
-
