e18246d7bbd488d1c510cbcd508ca9087204a6e2
Ori-Shoham <ori.shoham1@gmail.com>
2022-02-24 14:44:02 +0200

    debugging nd style improvement

--- R/save_to_lyx.R
+++ R/save_to_lyx.R
@@ -7,8 +7,6 @@
 #' @param digits a
 #' @param percent a
 #' @param accuracy a
-#' @param dataFile a
-#'
 #'
 #' @export
 #' @import data.table
@@ -17,7 +15,7 @@
 save_to_lyx <- function(currentValue, currentName, latexFile = dataLyxOutput, translate=TRUE,
                       digits = 2, percent=FALSE, accuracy=1) {
   # Test for valid inputs
-  if(any(!grepl("[^A-Za-z]", currentName))) {
+  if(any(grepl("[^A-Za-z]", currentName))) {
     stop("Names must consist of letters only")
   }
   if(any(duplicated(currentName))){
@@ -31,7 +29,7 @@ save_to_lyx <- function(currentValue, currentName, latexFile = dataLyxOutput, tr
   }
   n <- length(currentValue)
   # Format values
-  for (i in 1:n){
+  for (i in 1:n) {
     if (is.numeric(currentValue[i])) {
       if ((percent+rep(0,n))[i]) {
         currentValue[i] = scales::percent(currentValue[i], accuracy=accuracy, big.mark = ",")
@@ -40,7 +38,6 @@ save_to_lyx <- function(currentValue, currentName, latexFile = dataLyxOutput, tr
       }
     }
   }
-
 
   if (translate==TRUE) {
     currentValue = Hmisc::latexTranslate(currentValue)
@@ -51,7 +48,7 @@ save_to_lyx <- function(currentValue, currentName, latexFile = dataLyxOutput, tr
       DATA <- data.table::fread(latexFile, sep=" ", header = FALSE, col.names = c("name", "value"))
       #DATA[, name:=gsub("\\\\newcommand\\\\", "", name)]
       #DATA[, value:=gsub("^\\{", "", gsub("}$", "", value))]
-      DATA = DATA[gsub("\\\\newcommand\\\\", "", name)!=currentName]
+      DATA = DATA[!gsub("\\\\newcommand\\\\", "", name)%in% currentName]
   } else {
       DATA = data.table::data.table()
     }
@@ -64,13 +61,11 @@ save_to_lyx <- function(currentValue, currentName, latexFile = dataLyxOutput, tr
 
   first = 1; # iterateName=DATA$name[1]
   for(iterateName in DATA$name) {
-    myAppend = FALSE
-    if (first==0) {
-      myAppend = TRUE
-    }
-
+    myAppend <-  ifelse(first==0, TRUE, FALSE)
     iterateValue = DATA[name==iterateName, value]
-    write(file=latexFile, paste0(iterateName, " ", iterateValue), append=myAppend)
+    write(paste0(iterateName, " ", iterateValue), file=latexFile, append=myAppend)
     first = 0
   }
 }
+
+
