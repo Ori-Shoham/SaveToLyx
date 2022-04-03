e95473247ed36e7267c5390bb2dab7e444b785a1
Ori-Shoham <ori.shoham1@gmail.com>
2022-04-03 09:41:37 +0300

    Update test-save_to_lyx.R

--- tests/testthat/test-save_to_lyx.R
+++ tests/testthat/test-save_to_lyx.R
@@ -7,5 +7,9 @@ test_that("save_to_lyx does not accept invalid input",{
                  "Names must consist of letters only")
   expect_error(save_to_lyx(currentValue = 1:2, currentName = c("a","a"), latexFile = "a.tex"),
                "Names must be unique")
+  expect_error(save_to_lyx(currentValue = 1:2, currentName = "a", latexFile = "a.tex"),
+               "Names and Values must be of compatible lengths")
+  expect_error(save_to_lyx(currentValue = 1:3, currentName = letters[1:3], latexFile = "a.tex", percent = c(T,F)),
+               "percent must be of length 1 or of the same length as values and names")
 })
 
