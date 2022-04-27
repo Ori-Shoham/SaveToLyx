dd6e34cbb4996f5bf435b172517cf08871f8ef06
Ori-Shoham <ori.shoham1@gmail.com>
2022-04-27 16:25:25 +0300

    improve remove_from_lyx inner documentation

--- R/remove_from_lyx.R
+++ R/remove_from_lyx.R
@@ -10,7 +10,14 @@
 remove_from_lyx <- function(currentName, latexFile) {
 
   if(!file.exists(latexFile)) stop("File does not exist")
+
+  #technical solution to notes
+  name <- value <- NULL
+
+  # Read file
   DATA <- data.table::fread(latexFile, sep = " ", header = FALSE, col.names = c("name", "value"))
+
+  # Warn if any of the variable names is not in the file
   names_not_found <- NULL
   for(iterateName in currentName){
     if(!iterateName %in% gsub("\\\\newcommand\\\\", "",DATA$name)){
@@ -20,15 +27,16 @@ remove_from_lyx <- function(currentName, latexFile) {
   n <- length(names_not_found)
   if(!is.null(names_not_found)){
     if(n>1){
-      names_not_found <- paste0(paste(names_not_found[1:(n-1)], collapse = ", "),
-                                " & ", names_not_found[n])
+      names_not_found <- paste0('"', paste(names_not_found[1:(n-1)], collapse = '", "'),
+                                '& "' , names_not_found[n],'"')
       warning(paste0(names_not_found, " were not found in the file"))
     } else {
       warning(paste0(names_not_found, " was not found in the file"))
     }
   }
+  # Remove values from data
   DATA <- DATA[!gsub("\\\\newcommand\\\\", "", name) %in% currentName]
-  # Write values to file
+  # Write remaining values to file
   first <- 1 # iterateName=DATA$name[1]
   for (iterateName in DATA$name) {
     myAppend <- ifelse(first == 0, TRUE, FALSE)
